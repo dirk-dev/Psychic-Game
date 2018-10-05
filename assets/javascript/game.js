955c303098d8372022edc3d33a21ac84c8c2580d
Dirk Kiesewetter <dirk.k1@outlook.com>
2018-10-05 11:03:12 -0400

    changes to js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,6 @@
 // creates array that computer selects choice from
 let computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-
-var user
 // defines variables used in scoreboard
 let wins = 0;
 let losses = 0;

187fc5eeb55d86fe1e6aebf3d1e507468d7fb871
Dirk Kiesewetter <dirk.k1@outlook.com>
2018-10-05 12:46:38 -0400

    updated js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,7 @@
 // creates array that computer selects choice from
-let computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+let computerChoices = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+]
 
 // defines variables used in scoreboard
 let wins = 0;
@@ -12,14 +14,15 @@ console.log("letters guessed: " + letters_guessed);
 
 // computer randomly chooses from the computerChoices array
 //Math.random returns a number between 0 and 1, and Math.floor changes it to an integer 
-let computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
 // event occurs when user releases a key on the keyboard
 document.onkeyup = function (event) {
 
     // the user keystroke is the event; also ensures input is read as lower case
     let userGuess = event.key;
-    userGuess = userGuess.toLowerCase();
+    userGuess = userGuess.toUpperCase();
 
     // populates the letters_guessed array with user input for onscreen display
     letters_guessed.push(userGuess);
@@ -27,21 +30,24 @@ document.onkeyup = function (event) {
     // displays user & computer guesses to the console.log
     console.log("user guess = " + userGuess + " computer guess = " + computerGuess);
 
-    // user conditions for win, loss, 1 less guess, with code to reset guess count after win/loss
+    // user conditions for win
     if (userGuess === computerGuess) {
         wins++;
         guesses_left = 9;
         //resets letters_guessed array to 0
         letters_guessed.length = 0;
+        
 
-    } else if (userGuess !== computerGuess && guesses_left > 0) {
-        guesses_left--;
-
-    } else if (userGuess !== computerGuess && guesses_left === 0) {
+    // user conditions for loss
+    } else if (userGuess != computerGuess && guesses_left == 0) {
         losses++;
         guesses_left = 9;
         //resets letters_guessed array to 0
         letters_guessed.length = 0;
+
+    // user conditions for incorrect guess
+    } else  {
+        guesses_left--;
 
     }
     //updates the onscreen scoreboard

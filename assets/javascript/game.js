01a3d7cf6aa98f87c61565df308d4eab8050eb7c
Dirk Kiesewetter <dirk.k1@outlook.com>
2018-10-06 08:04:41 -0400

    updated js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,17 @@
 // creates array that computer selects choice from
-computerChoices = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+let letterArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 // defines variables used in scoreboard
 let wins = 0;
 let losses = 0;
-let guesses_left = 9;
+let guessesLeft = 9;
 
 //creates an array to store user guesses & logs to console
-let letters_guessed = [];
+let lettersGuessed = [];
 
 // computer randomly chooses from the computerChoices array
 //Math.random returns a number between 0 and 1, and Math.floor changes it to an integer 
-let computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+let computerGuess = letterArray[Math.floor(Math.random() * letterArray.length)];
 
 // event occurs when user releases a key on the keyboard
 document.onkeyup = function (event) {
@@ -21,7 +21,7 @@ document.onkeyup = function (event) {
     userGuess = userGuess.toUpperCase();
 
     // populates the letters_guessed array with user input for display in the "Your guesses so far: "
-    letters_guessed.push(userGuess);
+    lettersGuessed.push(userGuess);
 
     // displays user & computer guesses to the console.log
     console.log("user guess = " + userGuess + " computer guess = " + computerGuess);
@@ -29,25 +29,25 @@ document.onkeyup = function (event) {
     // user conditions for win
     if (userGuess === computerGuess) {
         wins++;
-        guesses_left = 9;
-        letters_guessed.length = 0;
+        guessesLeft = 9;
+        lettersGuessed.length = 0;
 
         // user conditions for loss
-    } else if (userGuess != computerGuess && guesses_left === 1) {
+    } else if (userGuess != computerGuess && guessesLeft === 1) {
         losses++;
-        guesses_left = 9;
-        letters_guessed.length = 0;
+        guessesLeft = 9;
+        lettersGuessed.length = 0;
 
         // incorrect guess reduces guesses left by 1
     } else {
-        guesses_left--;
+        guessesLeft--;
 
     }
 
     //updates the onscreen scoreboard
     document.getElementById('score_wins').innerHTML = "Wins: " + wins;
     document.getElementById('score_losses').innerHTML = "Losses: " + losses;
-    document.getElementById('guesses_left').innerHTML = "Guesses left: " + guesses_left;
-    document.getElementById('guesses_current').innerHTML = "Your guesses so far: " + letters_guessed;
+    document.getElementById('guesses_left').innerHTML = "Guesses left: " + guessesLeft;
+    document.getElementById('guesses_current').innerHTML = "Your guesses so far: " + lettersGuessed;
 
 }
